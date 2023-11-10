6ae55f8985723ab7c214d0300ddf1140ea234055
amellalalex <github@alexamellal.com>
2023-11-10 08:13:43 -0400

    First command: init now responding to cli.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::ffi::OsString;
+
+use clap::{arg, Command};
+
+fn cli() -> Command {
+    Command::new("bulgogi")
+        .about("A build system orchestrator for CubeSat projects")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_external_subcommands(true)
+        .subcommand(
+            Command::new("init")
+                .about("Initializes bulgogi project directory")
+                .arg(arg!(<PATH> "The directory to initialize"))
+                .arg_required_else_help(true),
+        )
+}
+
 fn main() {
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("init", sub_matches)) => {
+            println!(
+                "Initialized bulgogi project {}",
+                sub_matches.get_one::<String>("PATH").expect("required")
+            );
+        }
+        Some((ext, sub_matches)) => {
+            let args = sub_matches
+                .get_many::<OsString>("")
+                .into_iter()
+                .flatten()
+                .collect::<Vec<_>>();
+            println!("Calling out to {ext:?} with {args:?}");
+        }
+        _ => unreachable!(),
+    }
+
     println!("Hello, world!");
 }
