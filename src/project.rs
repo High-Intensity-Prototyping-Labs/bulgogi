8bcf55b105a5a5bd785b15169fd972d69c73d048
amellalalex <github@alexamellal.com>
2023-11-21 22:47:38 -0400

    Bunch of stuff -- not perfect but code is looking cleaner.

--- src/project.rs
+++ src/project.rs
@@ -1,11 +1,14 @@
 // Project module
 use crate::target::Target;
+use crate::target::TARGET_DEFAULT;
 use crate::dependency::{Dependency, DepKind};
 use crate::client;
 use crate::client::{InfoKind, HelpKind};
 
 use std::fs::File;
 use std::path::Path;
+
+const PROJECT_YAML: &str = "project.yaml";
 
 pub struct Project {
     targets: Vec<Target>,
@@ -21,12 +24,12 @@ impl Project {
     /// Initializes a bulgogi project in the PWD of the shell.
     pub fn init() {
         // Check if project.yaml exists 
-        if Path::new("project.yaml").exists() {
+        if Path::new(PROJECT_YAML).exists() {
             // Advise that project exists
             client::help(client::HelpKind::ProjectFound);
         } else {
             // Create project file with nothing in it
-            if let Ok(_) = File::create("project.yaml") {
+            if let Ok(_) = File::create(PROJECT_YAML) {
                 // Project initialized
                 client::info(InfoKind::InitSuccess);
             } else {
@@ -37,7 +40,67 @@ impl Project {
     }
 
     /// Adds a module to the project 
-    pub fn add_module(&mut self, module: &Dependency) {
+    ///
+    /// TODO: Here's the idea: the struct-level API should reflect the terminal.
+    ///         The project-level '.add_module' should simply add a module to the default target.
+    ///         If a specific target is needed, the target should first be .found() and *then* have 
+    ///         a module added. Otherwise, we are simply not respecting the object-model and doing
+    ///         a bunch of redundant searching and if-checking.
+    ///
+    ///     So the premise goes something like this: project.add_module() => adds module to default
+    ///                                             project.add_target() => adds empty target 
+    ///                                             target.add_module() => adds a module to this
+    pub fn add_module(&mut self, module_name: String) {
+        if let Some(target) = self.find_default_target_mut() {
+        // Default target exists -- simply add module to it 
+            target.deps.push((module_name, DepKind::Module));
+        } else {
+        // Default module not yet created -- make it
+            self.targets.push(Target { deps: vec![(module_name, DepKind::Module)], ..Default::default() });
+        }
+    }
 
+    /// Adds a target to the project 
+    pub fn add_target(&mut self, target_name: String) {
+        self.targets.push(Target::from(target_name));
+    }
+
+    /// Returns a ref to a target which matches the criteria
+    pub fn find_target(&self, name: &String) -> Option<&Target> {
+        for target in &self.targets {
+            if &target.name == name {
+                return Some(target);
+            }
+        }
+        None
+    }
+
+    /// Returns a mut ref to a target with `name`
+    pub fn find_target_mut(&mut self, name: &String) -> Option<&mut Target> {
+        for target in &mut self.targets {
+            if &target.name == name {
+                return Some(target);
+            }
+        }
+        None
+    }
+
+    /// Returns a ref to the default target in the project 
+    pub fn find_default_target(&self) -> Option<&Target> {
+        return self.find_target(&Target::default().name);
+    }
+
+    /// Returns a mut ref to the default target in the project 
+    pub fn find_default_target_mut(&mut self) -> Option<&mut Target> {
+        return self.find_target_mut(&Target::default().name);
+    }
+
+    /// Saves the project to disk 
+    pub fn save(&self) -> () {
+        if let Ok(f) = File::options().write(true).open(PROJECT_YAML) {
+        // Project file exists -- overwrite previous config
+        } else {
+        // Project file does not exist
+        }
     }
 }
--- src/target.rs
+++ src/target.rs
@@ -2,7 +2,27 @@
 
 use crate::dependency::Dependency;
 
+pub const TARGET_DEFAULT: &str = "default";
+
 pub struct Target {
     pub name: String,
     pub deps: Vec<Dependency>,
+}
+
+impl Default for Target {
+    fn default() -> Self {
+        Target {
+            name: String::from(TARGET_DEFAULT),
+            deps: vec![],
+        }
+    }
+}
+
+impl From<String> for Target {
+    fn from(value: String) -> Self {
+        Target {
+            name: value,
+            ..Default::default()
+        }
+    }
 }
