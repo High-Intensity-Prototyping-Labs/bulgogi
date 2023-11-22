0a4c6f08321b1c75b91feb0576d5a2afeb50630f
amellalalex <github@alexamellal.com>
2023-11-22 16:28:35 -0400

    Added bulgogi add module clause to the client.

--- src/dependency.rs
+++ src/dependency.rs
@@ -1,4 +1,7 @@
 // Dependency Module
+
+pub const MODULE_DEFAULT: &str = "module1";
+
 pub type Dependency = (String, DepKind);
 
 pub enum DepKind {
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod dependency;
 mod client;
 
 use crate::project::Project;
+use crate::target::TARGET_DEFAULT;
+use crate::dependency::MODULE_DEFAULT;
 
 fn main() {
     let matches = client::cli().get_matches();
@@ -13,6 +15,15 @@ fn main() {
         Some(("init", _)) => {
             // Initialize project in the PWD
             Project::init();
+        }
+        Some(("module", cmd)) => {
+            match cmd.subcommand() {
+                Some(("add", sub)) => {
+                    let module = sub.get_one::<String>("MODULE").unwrap_or(&String::from(MODULE_DEFAULT));
+                    let target = sub.get_one::<String>("TARGET").unwrap_or(&String::from(TARGET_DEFAULT));
+                }
+                _ => unreachable!(),
+            }
         }
         _ => unreachable!(),
     }
