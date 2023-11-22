4f0c37e249844adefece74aed9f1366df463c022
amellalalex <github@alexamellal.com>
2023-11-22 17:33:10 -0400

    Added ToDo as I attempted to implement the module add clause in the client.

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,24 @@ fn main() {
         Some(("module", cmd)) => {
             match cmd.subcommand() {
                 Some(("add", sub)) => {
+                    // Placehold add command arguments
                     let module = sub.get_one::<String>("MODULE").unwrap_or(&String::from(MODULE_DEFAULT));
                     let target = sub.get_one::<String>("TARGET").unwrap_or(&String::from(TARGET_DEFAULT));
+
+                    // Load project
+                    let mut project = Project::load().unwrap_or(Project::new());
+
+                    // Add module
+                    project.add_target(target);
+
+                    // TODO:
+                    // Come to terms with how the high-level interface is fighting with itself;
+                    // The project methods are a little too independent because a lot of the
+                    // default behaviour can be handled up here.
+                    //
+                    // Decide to either:
+                    //  1. Handle less defaults and assumptions in the Project interface 
+                    //  2. Do much less up here
                 }
                 _ => unreachable!(),
             }
