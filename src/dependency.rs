f1460db3079e9f6c07685cde573f47bb6ae94dec
amellalalex <github@alexamellal.com>
2023-12-06 19:30:14 -0400

    Converted Dependency to struct and added DepFlag field.

--- src/dependency.rs
+++ src/dependency.rs
@@ -2,11 +2,42 @@
 
 pub const MODULE_DEFAULT: &str = "module1";
 
-pub type Dependency = (String, DepKind);
+/// Target dependency type
+#[derive(Debug, Clone, PartialEq)]
+pub struct Dependency {
+    pub name: String,
+    pub kind: DepKind,
+    pub flag: DepFlag,
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum DepKind {
     Target,
     Module,
-    ModuleExe,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum DepFlag {
+    None,
+    Executable,
+}
+
+impl From<(String, DepKind)> for Dependency {
+    fn from(value: (String, DepKind)) -> Self {
+        Dependency {
+            name: value.0,
+            kind: value.1,
+            flag: DepFlag::None,
+        }
+    }
+}
+
+impl From<(String, DepKind, DepFlag)> for Dependency {
+    fn from(value: (String, DepKind, DepFlag)) -> Self {
+        Dependency {
+            name: value.0,
+            kind: value.1,
+            flag: value.2,
+        }
+    }
 }
