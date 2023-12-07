7d9b2ccfbdbb0747508c493399cf1c6023a1c2c5
amellalalex <github@alexamellal.com>
2023-12-07 09:28:49 -0400

    Logical framework for CMake module.

--- src/cmake.rs
+++ src/cmake.rs
@@ -1,37 +1,26 @@
 // CMake module
 
 use crate::project::Project;
+use crate::target::Target;
+use crate::dependency::DepKind;
 
+/// The largest unit the CMake module understands.
+/// Designed to be interchangeable with bulgogi projects.
 pub struct CMakeProject {
-    pub targets: Vec<CMakeTarget>
+    pub lists: Vec<CMakeList>
 }
 
-pub struct CMakeTarget {
-    pub kind: CMakeTargetKind,
+/// Grouping of CMakeTargets -- these are better representations of actual compiled targets.
+/// (i.e.: every executable (.out) and library (.a) will have a CMakeList of CMakeTargets that are
+/// linked together.
+pub struct CMakeList {
+    pub targets: Vec<CMakeTarget>,
 }
 
-pub enum CMakeTargetKind {
-    LibModule,
-    LibTarget,
-    ExeTarget,
-}
-
-impl CMakeProject {
-    pub fn new() -> Self {
-        CMakeProject {
-            targets: vec![],
-        }
-    }
-}
-
-impl From<Project> for CMakeProject {
-    fn from(project: Project) -> Self {
-        let mut cmake_project = CMakeProject::new();
-
-        for target in project.targets {
-            
-        }
-
-        cmake_project
-    }
+/// Distinguishes libraries and executables apart.
+/// The first string is always the module (dir) name, the second is always the proxy target name.
+/// These two will often be the same value in the case of LibModules.
+pub enum CMakeTarget {
+    Library(String, String),
+    Executable(String, String),
 }
