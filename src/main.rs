f09addc253771fe5e97c5adaf3f7e66b8935aa43
amellalalex <github@alexamellal.com>
2023-12-16 18:38:26 -0400

    Disabled the rest of the project to make this compilable.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,20 @@
 mod project;
 //mod target;
 //mod dependency;
-mod client;
-mod template;
-mod cmake;
+//mod client;
+//mod template;
+//mod cmake;
 
 use std::io;
+use std::fs;
+
+use serde_yaml::Mapping;
+use project::Project;
 
 fn main() -> Result<(), io::Error> {
-    client::next_command()?;
+//    client::next_command()?;
+    let file = fs::File::open("project.yaml")?;
+    let map: Mapping = serde_yaml::from_reader(file).unwrap();
+    let project = Project::from(map);
     Ok(())
 }
