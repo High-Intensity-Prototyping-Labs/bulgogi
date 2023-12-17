cdbb2c055a4837e4c26c47e8cc3348738e642bd9
amellalalex <github@alexamellal.com>
2023-12-17 16:54:57 -0400

    Re-integrated client module into main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,12 @@
 mod project;
 //mod target;
 //mod dependency;
-//mod client;
+mod client;
 //mod template;
 //mod cmake;
 
 use std::io;
-use std::fs;
-
-use serde_yaml::Mapping;
-use project::Project;
-
 fn main() -> Result<(), io::Error> {
-//    client::next_command()?;
-    let file = fs::File::open("project.yaml")?;
-    let map: Mapping = serde_yaml::from_reader(file).unwrap();
-    let project = Project::from(map);
+    client::next_command()?;
     Ok(())
 }
