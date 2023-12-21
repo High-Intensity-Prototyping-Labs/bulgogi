47a24ef480b7fd5d17cd302a19044c2f1fbabcde
amellalalex <github@alexamellal.com>
2023-12-21 19:08:31 -0400

    Way better configuration of CMake module.

--- src/cmake.rs
+++ src/cmake.rs
@@ -4,54 +4,19 @@ use crate::project::{Project, TargetID, Target, Dependency, Module, ModuleID, Pr
 
 use std::collections::HashMap;
 
-pub type SubmoduleDir = String;
-pub type LibraryName = String;
-pub type ExecutableName = String;
+pub type Submodule = String;
 
-#[derive(Debug)]
 pub struct CMakeProject {
-    pub submodules: HashMap<SubmoduleDir, CMakeList>,
+    pub submodules: Vec<Submodule>,
+    pub lists: HashMap<Submodule, CMakeList>,
 }
 
-#[derive(Debug)]
 pub struct CMakeList {
     pub target: CMakeTarget,
-    pub links: Vec<LibraryName>,
+    pub links: Option<>,
 }
 
-#[derive(Debug)]
 pub enum CMakeTarget {
-    Library(LibraryName),
-    Executable(ExecutableName),
-}
-
-impl CMakeProject {
-    pub fn new() -> Self {
-        CMakeProject {
-            submodules: HashMap::new(),
-        }
-    }
-}
-
-impl From<Project> for CMakeProject {
-    fn from(project: Project) -> Self {
-        let proxy_libs = project.deps.iter()
-            .map(|(target_id, dep_list)| (target_id, dep_list.module_ids()))
-            .filter_map(|(target_id, mod_list)| filter_match!(mod_list.first(), Some(m), Some((target_id.clone(), m.clone()))))
-            .collect::<HashMap<TargetID, SubmoduleDir>>();
-
-        //CMakeProject {
-        //    submodules,
-        //}
-        CMakeProject::new()
-    }
-}
-
-impl From<(TargetID, Target)> for CMakeTarget {
-    fn from(entry: (TargetID, Target)) -> Self {
-        match entry.1 {
-            Target::Library => CMakeTarget::Library(entry.0),
-            Target::Executable => CMakeTarget::Executable(entry.0),
-        }
-    }
+    Library(String),
+    Executable(String),
 }
