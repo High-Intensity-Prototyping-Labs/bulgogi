041dea7bb62b324042033bd4bc0ae9760519e20c
amellalalex <github@alexamellal.com>
2023-12-22 16:12:01 -0400

    Almost done collecting lists for Project to CMakeProject conversion.

--- src/cmake.rs
+++ src/cmake.rs
@@ -13,10 +13,51 @@ pub struct CMakeProject {
 
 pub struct CMakeList {
     pub target: CMakeTarget,
-    pub links: Option<>,
+    pub links: Option<Vec<CMakeTarget>>,
 }
 
 pub enum CMakeTarget {
     Library(String),
     Executable(String),
+}
+
+impl CMakeProject {
+    pub fn new() -> Self {
+        CMakeProject {
+            submodules: Vec::new(),
+            lists: HashMap::new(),
+        }
+    }
+}
+
+impl From<Project> for CMakeProject {
+    fn from(project: Project) -> Self {
+        let submodules = project.modules.keys().collect();
+        let lists = project.deps.iter()
+            .filter_map(|(target_id, dep_list)| filter_match!(project.targets.get_key_value(target_id), Some(entry), Some((entry, dep_list))))
+            .map(|(entry, dep_list)| {
+                CMakeList {
+                    target: CMakeTarget::from((entry.0.clone(), entry.1.clone())),
+                    links: ,
+                }
+            })
+    }
+}
+
+impl From<(TargetID, Target)> for CMakeTarget {
+    fn from((target_id, target): (TargetID, Target)) -> Self {
+        match target {
+            Target::Library => CMakeTarget::Library(target_id),
+            Target::Executable => CMakeTarget::Executable(target_id),
+        }
+    }
+}
+
+impl From<Dependency> for CMakeTarget {
+    fn from(dep: Dependency) -> Self {
+        match dep {
+            Dependency::Module(m) => CMakeTarget::Library(m),
+            Dependency::Target(t) => CMakeTarget::Library(t),
+        }
+    }
 }
