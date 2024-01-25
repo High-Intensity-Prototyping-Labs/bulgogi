4f1e9422efe6cf56b963c6622682fde38ef03a8d
amellalalex <github@alexamellal.com>
2024-01-25 21:08:59 +0100

    Removed previously removed functions from main.c

--- src/main.c
+++ src/main.c
@@ -28,8 +28,6 @@ int main(void) {
         int done  = 0;
         int count = 0;
         int error = 0;
-        int valid = 0;
-        char *msg = NULL;
 
         engine = bul_engine_init();
 
@@ -62,20 +60,10 @@ int main(void) {
         yaml_parser_delete(&parser);
         assert(!fclose(file));
 
-        printf("#-------------------- Engine pre-resolution\n");
         bul_engine_print(&engine);
-
-        bul_engine_resolve_ambiguity(&engine);
-
-        printf("#-------------------- Engine post-resolution\n");
-        bul_engine_print(&engine);
-
-        msg = bul_engine_assert_valid(&engine);
-        valid = (msg == NULL);
 
         bul_engine_free(&engine);
 
         printf("%s (%d events)\n", (error ? "FAILURE" : "SUCCESS"), count);
-        printf("%s (bul_engine)\n", (valid ? "OK" : msg));
         return 0;
 }
