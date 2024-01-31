a31e0a217b1ad462398271e61c216fb8c60bdd5d
amellalalex <github@alexamellal.com>
2024-01-31 13:14:21 +0100

    Added documentation for bul_fs_mkdir

--- inc/bul_fs.h
+++ inc/bul_fs.h
@@ -22,7 +22,12 @@ typedef enum {
         BUL_FS_UNKNOWN,
 } bul_fs_status_t;
 
+/**
+ * @brief Wrapper for standard `mkdir()`.
+ *
+ * @param[in] path Path to directory to create.
+ * @return `BUL_FS_OK` in case success, see `bul_fs_status_t` otherwise.
+ */
 bul_fs_status_t bul_fs_mkdir(bul_fs_path_t path);
-bul_fs_status_t bul_cerr_to_fs_status(int errno);
 
 #endif // BUL_FS_H
