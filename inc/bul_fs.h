05aed9d582172f23b58eb48eadc674d09572c6d6
amellalalex <github@alexamellal.com>
2024-01-31 13:14:40 +0100

    Added the bul_fs_join function.

--- inc/bul_fs.h
+++ inc/bul_fs.h
@@ -8,6 +8,7 @@
 
 // Settings 
 #define DEFAULT_FS_MODE 0777
+#define DEFAULT_FS_SEP "/"
 
 typedef char* bul_fs_path_t;
 
@@ -29,5 +30,22 @@ typedef enum {
  * @return `BUL_FS_OK` in case success, see `bul_fs_status_t` otherwise.
  */
 bul_fs_status_t bul_fs_mkdir(bul_fs_path_t path);
+
+/**
+ * @brief Joins two paths together.
+ *
+ * WARNING:
+ * This function returns a `malloc()`'d string which 
+ * must be freed when discarded.
+ *
+ * ASSUMPTIONS:
+ * 1. There are no leading or trailing path separators.
+ * 2. Both paths are null-terminated.
+ *
+ * @param[in] a Parent path (left of separator).
+ * @param[in] b Child path (right of separator).
+ * @return Joint paths.
+ */
+bul_fs_path_t bul_fs_join(bul_fs_path_t a, bul_fs_path_t b);
 
 #endif // BUL_FS_H
--- src/bul_fs.c
+++ src/bul_fs.c
@@ -9,6 +9,10 @@
 #include <sys/errno.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 bul_fs_status_t bul_fs_mkdir(bul_fs_path_t path) {
         int res = 0;
@@ -20,4 +24,18 @@ bul_fs_status_t bul_fs_mkdir(bul_fs_path_t path) {
         } else {
                 return BUL_FS_OK;
         }
+}
+
+bul_fs_path_t bul_fs_join(bul_fs_path_t a, bul_fs_path_t b) {
+        size_t alen = 0;
+        size_t blen = 0;
+        bul_fs_path_t joint = NULL;
+
+        alen = strlen(a);
+        blen = strlen(b);
+
+        joint = malloc(alen + blen + 1);
+        sprintf(joint, "%s%s%s", a, DEFAULT_FS_SEP, b);
+
+        return joint;
 }
