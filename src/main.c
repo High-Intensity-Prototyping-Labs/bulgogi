47c7618122ef98b7aacffed39b49516c6cca460d
amellalalex <github@alexamellal.com>
2024-01-31 15:40:28 +0100

    Adjusted main to either invoke a subcommand or add a target.

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Project Headers 
 #include "fs.h"
@@ -48,14 +49,28 @@ static void load_and_print_project(void) {
 }
 
 static void test_dot_bul_features(char *argv[]) {
-        printf("Creating target (%s)\n", argv[1]);
-        bul_dot_add_target(argv[1]);
+        printf("Creating target (%s)\n", argv[2]);
+        bul_dot_add_target(argv[2]);
+}
+
+static void some_subcommand(char *argv[]) {
+        if(strcmp(argv[1], "init") == 0) {
+                bul_dot_init();
+                printf("Initialized .bul directory.\n");
+        } else if(strcmp(argv[1], "add") == 0) {
+                printf("Missing a target name\n");
+                printf("Try: bul add <TARGET>\n");
+        } else {
+                printf("Not exactly a subcommand...\n");
+        }
 }
 
 int main(int argc, char *argv[]) {
         if(argc == 1) {
                 load_and_print_project();
         } else if(argc == 2) {
+                some_subcommand(argv);
+        } else if(argc == 3) {
                 test_dot_bul_features(argv);
         } else {
                 printf("Lovely day, but I don't recognize that number of args.\n");
