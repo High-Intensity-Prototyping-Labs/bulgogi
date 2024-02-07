8437507deaf2ad758497d7cee90142d885f6efe7
amellalalex <github@alexamellal.com>
2024-02-07 23:46:47 +0100

    Got the new core up until a certain point (see notes).
    Most of the basis is good but I seem to have hit a wall.
    Specifically, right around how to deal with 'unwinding' when a DOCUMENT_END or a SEQUENCE_END is encountered.
    It is much easier to cap the level of instrospection to 1 because it means a single 'previous' variable can track the way back.
    
    Current ideas revolve around a **parents array in core or having each target struct store its parent.

--- inc/core.h
+++ inc/core.h
@@ -8,6 +8,13 @@
 
 // Standard C Libraries
 #include <stddef.h>
+#include <limits.h>
+
+// External Dependencies 
+#include "yaml.h"
+
+#define BUL_DOC_NAME "DOCUMENT"
+#define BUL_MAX_ID UINT_MAX
 
 typedef unsigned int bul_id_t;
 
@@ -20,9 +27,11 @@ struct bul_target {
 typedef struct bul_target bul_target_s;
 
 struct bul_core {
-        unsigned int level;
-        bul_target_s *targets;
         size_t size;
+        size_t level;
+        bul_id_t scope;
+        bul_id_t *parents;
+        bul_target_s *targets;
 };
 typedef struct bul_core bul_core_s;
 
@@ -30,15 +39,65 @@ typedef struct bul_core bul_core_s;
  * General assumptions for these functions:
  * 1. Pointers are non-NULL.
  * 2. All structs are initialized.
- * 3. All C-library memory initializations work.
+ * 3. Configuration files are parsed sequentially.
  */
 
 /**
  * @brief Initializes a core struct.
  *
+ * WARNINGS:
+ * 1. This function uses `malloc()` 
+ * 2. The struct must subsequently be freed using `bul_core_free()`.
+ *
  * @return An initialized core struct.
  */
 bul_core_s bul_core_init(void);
+
+/**
+ * @brief Processes the next YAML event.
+ *
+ * ASSUMPTIONS:
+ * 1. YAML events are parsed sequentially from start to finish.
+ *
+ * @param[in] core Core context to use.
+ * @param[in] event Next YAML event to parse.
+ */
+void bul_core_next_event(bul_core_s *core, yaml_event_t *event);
+
+/**
+ * @brief Processes a YAML event of type `YAML_DOCUMENT_START_EVENT`.
+ *
+ * @param[in] core Core context to use.
+ * @param[in] event Event of type `YAML_DOCUMENT_START_EVENT`.
+ */
+void bul_core_document_start(bul_core_s *core, yaml_event_t *event);
+
+/**
+ * @brief Processes a YAML event of type `YAML_DOCUMENT_END_EVENT`.
+ *
+ * @param[in] core Core context to use.
+ * @param[in] event Event of type `YAML_DOCUMENT_END_EVENT`.
+ */
+void bul_core_document_end(bul_core_s *core, yaml_event_t *event);
+
+/**
+ * @brief Increases the size of the core's capacity by 1.
+ *
+ * @param[in] core Core context to grow.
+ */
+void bul_core_grow(bul_core_s *core);
+
+/**
+ * @brief Adds a target to the core.
+ *
+ * ASSUMPTIONS:
+ * 1. An existing (duplicate) target does not already exist.
+ *
+ * @param[in] core Core context to use.
+ * @param[in] name Name of the target to add.
+ * @return bul_id_t ID of the newly added target.
+ */
+bul_id_t bul_core_target_add(bul_core_s *core, char *name);
 
 /**
  * @brief Frees an initialized core struct.
@@ -46,5 +105,32 @@ bul_core_s bul_core_init(void);
  * @param[in] core Core struct to initialize.
  */
 void bul_core_free(bul_core_s *core);
+
+/**
+ * @brief Initializes a target struct.
+ *
+ * WARNINGS:
+ * 1. This target uses `strdup()` which allocates dynamic memory.
+ *
+ * @param[in] id ID of the target to initialize.
+ * @param[in] name Names of the target to initialize.
+ * @return The initialized target.
+ */
+bul_target_s bul_target_init(bul_id_t id, char *name);
+
+/**
+ * @brief Adds a child target to `target`.
+ *
+ * @param[in] target Target to add the child to.
+ * @param[in] child_id ID of the to add.
+ */
+void bul_target_add_child(bul_target_s *target, bul_id_t child_id);
+
+/**
+ * @brief Grows the child capacity of the target by 1.
+ *
+ * @param[in] target Target to grow.
+ */
+void bul_target_grow(bul_target_s *target);
 
 #endif // BUL_CORE_H
--- src/core.c
+++ src/core.c
@@ -5,6 +5,7 @@
 
 #include "core.h"
 #include <malloc/_malloc.h>
+#include <string.h>
 
 // Standard C Libraries 
 
@@ -16,12 +17,102 @@ bul_core_s bul_core_init(void) {
         bul_core_s core = {
                 .size = 0,
                 .level = 0,
+                .prevs = BUL_MAX_ID,
+                .scope = BUL_MAX_ID,
                 .targets = targets,
         };
 
         return core;
 }
 
+void bul_core_next_event(bul_core_s *core, yaml_event_t *event) {
+        switch(event->type) {
+        case YAML_DOCUMENT_START_EVENT:
+                bul_core_document_start(core, event);
+                break;
+        case YAML_DOCUMENT_END_EVENT:
+                bul_core_document_end(core, event);
+                break;
+        default:
+                break;
+        }
+}
+
+void bul_core_document_start(bul_core_s *core, yaml_event_t *event) {
+        bul_id_t id = 0;
+
+        // Add document as a target.
+        id = bul_core_target_add(core, BUL_DOC_NAME);
+
+        // Set scope and update level 
+        core->prevs = core->scope;
+        core->scope = id;
+        core->level++;
+}
+
+void bul_core_document_end(bul_core_s *core, yaml_event_t *event) {
+        core->scope = core->prevs;
+        core->level--;
+}
+
+void bul_core_grow(bul_core_s *core) {
+        core->size++;
+        core->targets = realloc(core->targets, (core->size+1) * sizeof(bul_target_s));
+        /* capacity is size+1 */
+}
+
+bul_id_t bul_core_target_add(bul_core_s *core, char *name) {
+        bul_id_t     id = 0;
+        bul_target_s target;
+
+        id     = core->size;
+        target = bul_target_init(id, name);
+
+        bul_core_grow(core);
+
+        core->targets[id] = target;
+
+        if(core->level > 0) {
+                // Add to scope
+                bul_target_add_child(core->targets + core->scope, id);
+        }
+
+        return id;
+}
+
 void bul_core_free(bul_core_s *core) {
         free(core->targets);
+}
+
+bul_target_s bul_target_init(bul_id_t id, char *name) {
+        char *name_dup = NULL;
+        bul_id_t *children = NULL;
+
+        name_dup = strdup(name);
+        children = malloc(sizeof(bul_id_t));
+
+        bul_target_s target = {
+                .id = id,
+                .name = name_dup,
+                .size = 0,
+                .children = NULL,
+        };
+
+        return target;
+}
+
+void bul_target_add_child(bul_target_s *target, bul_id_t child_id) {
+        bul_id_t child_num = 0;
+
+        child_num = target->size;
+
+        bul_target_grow(target);
+        
+        target->children[child_num] = child_id;
+}
+
+void bul_target_grow(bul_target_s *target) {
+        target->size++;
+        target->children = realloc(target->children, (target->size+1) * sizeof(bul_id_t));
+        /* target capacity is size+1 */
 }
